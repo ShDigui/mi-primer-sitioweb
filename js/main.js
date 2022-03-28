d86f257f92cfc312b01f3edfb7aee8045e7f8242
ShDigui <diegosalazae2003@gmail.com>
2022-03-28 03:43:13 -1200

    menu

--- js/main.js
+++ js/main.js
@@ -2,23 +2,23 @@ $(document).ready(main);
 
 var contador = 0;
 
-function main () {
-	$('.menu_bar').click( function (){
-		if (contador == 0) {
-			$('nav').animate({
-				left: '0'
-			});
-			contador = 1;
-		} else {
-			$('nav').animate({
-				left: '-100%'
-			});
-			contador=0;
-		}
-	});
-
-	// Mostramos y ocultamos submenus
-	$('.submenu').click(function(){
-		$(this).children('.children').slideToggle();
-	});
+function main() {
+    $('.menu-bar').click(funcion() { 
+        if (contador == 0) {
+             $('nav').animate({
+                 left: '0'
+             });
+             contador =1;
+         }
+        else {
+             $('nav').animate({
+                 left: '-100%'
+             });
+             contador = 0;
+        }
+    });
+    $('.submenu').click(funcion(){
+        $(this).children('.children').slideToggle();
+    });
 }
+
